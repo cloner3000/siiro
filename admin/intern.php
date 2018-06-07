f3cb9bb0097b371d85636e60a842552de219eff6
Eizan <eizan38@gmail.com>
2018-06-07 15:43:08 +0700

    update

--- admin/intern.php
+++ admin/intern.php
@@ -9,7 +9,7 @@ include "../include/header.php";
 	$xcrud->table_name('Data Intern');
 	$xcrud->change_type('photo', 'image');
 	$xcrud->validation_required('name,periode_tahun');
-	$xcrud->change_type('periode_tahun', 'int');
+	$xcrud->change_type('periode_tahun', 'year');
 	$xcrud->change_type('passport_scan', 'file');
 	$xcrud->change_type('visa_scan', 'file');
     echo $xcrud->render();
