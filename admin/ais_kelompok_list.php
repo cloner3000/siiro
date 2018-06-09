72326161b642ba991ab1a0879bf0bfab6d8a3983
Eizan <eizan38@gmail.com>
2018-06-09 12:26:49 +0700

    update

--- admin/ais_kelompok_list.php
+++ admin/ais_kelompok_list.php
@@ -7,9 +7,8 @@ include "../include/header.php";
 </div>
 <br>
 
-<div class="w3-row-padding">
 
-<div class="w3-padding w3-card w3-quarter">
+<div class="w3-padding w3-border w3-quarter">
 	<b>Kelompok 1</b>
 	<p>Pembimbing</p>
 	<ul>
@@ -22,20 +21,7 @@ include "../include/header.php";
 	</ol>
 </div>
 
-<div class="w3-padding w3-card w3-quarter">
-	<b>Kelompok 1</b>
-	<p>Pembimbing</p>
-	<ul>
-		<li>Dudih Gustian</li>
-	</ul>
-	<p>Anggota</p>
-	<ol>
-		<li>Anton permana</li>
-		<li>Ikhsan Thoir</li>
-	</ol>
-</div>
 
-</div>
 
 <?php 
 include "../include/footer.php";
